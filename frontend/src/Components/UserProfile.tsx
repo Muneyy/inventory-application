2693793e4a8c9d619674ed704a8b93b7ee3a22b7
Muney <muneyyyy@gmail.com>
2022-12-02 17:20:07 +0800

    Fix bug: picture not being fetched in user profile

--- frontend/src/Components/UserProfile.tsx
+++ frontend/src/Components/UserProfile.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { Spinner, Container, Heading, Center, Text, Button, Stack, Link, Grid, FormLabel, FormControl, useDisclosure, Collapse, Box, Flex, GridItem, Circle, Avatar, AvatarBadge, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input, Alert, AlertDescription, AlertIcon, AlertTitle } from '@chakra-ui/react'
+import { Spinner, Container, Image, Heading, Center, Text, Button, Stack, Link, Grid, FormLabel, FormControl, useDisclosure, Collapse, Box, Flex, GridItem, Circle, Avatar, AvatarBadge, Drawer, DrawerBody, DrawerCloseButton, DrawerContent, DrawerFooter, DrawerHeader, DrawerOverlay, Input, Alert, AlertDescription, AlertIcon, AlertTitle } from '@chakra-ui/react'
 import {ArrowForwardIcon, CheckIcon} from '@chakra-ui/icons'
 import { login, logout } from '../Features/currentUserSlice';
 import { useAppSelector, useAppDispatch } from '../app/hooks'
@@ -23,6 +23,7 @@ function UserProfile () {
         handle : string,
         bio: string,
         _id: string,
+        avatarURL: string,
     }
 
     // Retrieve logged in user state and JWT token from Redux
@@ -105,14 +106,26 @@ function UserProfile () {
                     ? (
                         <Center backgroundColor={"teal.300"} p={10}>
                             <Box borderColor={"blackAlpha.300"} borderWidth='3px' borderRadius='lg' overflow='hidden' paddingX={15} paddingY={5}>
-                                <Grid templateColumns={"2fr 5fr"} gap={4}>
+                                <Grid templateColumns={"230px 1fr"} gap={4}>
                                     <GridItem display={"flex"} alignItems="center" justifyContent={"center"} p={5}>
-                                        <Avatar size={"xl"}></Avatar>
+                                        {(fetchedUser.avatarURL) 
+                                            ? (
+                                                <Image
+                                                    borderRadius='full'
+                                                    boxSize='200px'
+                                                    src={fetchedUser.avatarURL}
+                                                    objectFit="cover"
+                                                    alt='Avatar'/>
+                                            ) 
+                                            : (
+                                                <Avatar size={"md"}></Avatar>
+                                            )}
                                     </GridItem>
                                     <GridItem display={"flex"} justifyContent="center" alignItems="start" p={5} flexDir="column">
                                         <Text fontSize="5xl" fontWeight={700}>{fetchedUser.username}</Text>
                                         <Text fontSize={"sm"} fontWeight={300}>@{fetchedUser.handle}</Text>
                                         <Text fontSize={"md"} fontWeight={500}>{fetchedUser.bio}</Text>
+                                        {/* TODO: remove add friend option if user is already a friend */}
                                         <Button mt={1}  colorScheme="pink" ref={btnRef} onClick={() => sendFriendRequest(fetchedUser._id)} size="sm" disabled={friendRequestSent}>
                                             {friendRequestSent ? "Sent" : "Add Friend"}
                                         </Button>
